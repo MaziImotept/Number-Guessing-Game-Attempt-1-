dd4de21ad3c786785efa9af8e22f372499102137
SCOTTIE STERLING <sCOSTERL@UAT.EDU>
2018-04-20 21:05:10 -0700

    Add project files.

---
+++ Number Guessing Game/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Number_Guessing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_guess = new System.Windows.Forms.Button();
+            this.textbox_guess = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_guess
+            // 
+            this.button_guess.Location = new System.Drawing.Point(12, 107);
+            this.button_guess.Name = "button_guess";
+            this.button_guess.Size = new System.Drawing.Size(282, 47);
+            this.button_guess.TabIndex = 0;
+            this.button_guess.Text = "Guess!";
+            this.button_guess.UseVisualStyleBackColor = true;
+            this.button_guess.Click += new System.EventHandler(this.button_guess_Click);
+            // 
+            // textbox_guess
+            // 
+            this.textbox_guess.Location = new System.Drawing.Point(12, 81);
+            this.textbox_guess.Name = "textbox_guess";
+            this.textbox_guess.Size = new System.Drawing.Size(282, 20);
+            this.textbox_guess.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 166);
+            this.Controls.Add(this.textbox_guess);
+            this.Controls.Add(this.button_guess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_guess;
+        private System.Windows.Forms.TextBox textbox_guess;
+    }
+}
+
---
+++ Number Guessing Game/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_Guessing_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int number;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //generate the number
+            Random generator = new Random();
+            number = generator.Next(0, 101);
+            MessageBox.Show("I'm thinking of a number from 1-100, let's guess it!");
+        }
+
+        private void button_guess_Click(object sender, EventArgs e)
+        {
+            // get the guess from the textbox
+            int guess = Convert.ToInt32(textbox_guess.Text);
+
+            //check the guess
+            if (guess > number)
+            {
+                MessageBox.Show("Too High.");
+            }
+            if (guess < number)
+            {
+                MessageBox.Show("Too low.");
+            }
+            if (guess == number)
+            {
+                MessageBox.Show("You guessed it.");
+                Application.Exit();
+            }
+        }
+    }
+}
